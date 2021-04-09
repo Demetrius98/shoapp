764fcc0524bf89591181afab15540c76b3c02615
dmitry <dmitry.bushmanov@gmail.com>
2021-04-09 11:28:56 +0300

    init Spring application

---
+++ src/main/java/com/javaMessenger/Messenger/MessengerApplication.java
@@ -0,0 +1,14 @@
+package com.javaMessenger.Messenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessengerApplication.class, args);
+	}
+
+
+}
