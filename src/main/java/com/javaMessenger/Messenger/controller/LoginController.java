c6e23c2a7b53537f4081a33df33a45bc62b1a885
dmitry <dmitry.bushmanov@gmail.com>
2021-04-14 20:20:06 +0300

    Add Login Controller

---
+++ src/main/java/com/javaMessenger/Messenger/controller/LoginController.java
@@ -0,0 +1,116 @@
+package com.javaMessenger.Messenger.controller;
+
+import com.javaMessenger.Messenger.domain.User;
+import com.javaMessenger.Messenger.service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.util.List;
+
+/**
+ *
+ * @author dmitry
+ */
+@Controller
+public class LoginController {
+
+    @Autowired
+    private CustomUserDetailsService userService;
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public ModelAndView login() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("login");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/signup", method = RequestMethod.GET)
+    public ModelAndView signup() {
+        ModelAndView modelAndView = new ModelAndView();
+        User user = new User();
+        modelAndView.addObject("user", user);
+        modelAndView.setViewName("signup");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/signup", method = RequestMethod.POST)
+    public ModelAndView createNewUser(@Valid User user, BindingResult bindingResult) {
+        ModelAndView modelAndView = new ModelAndView();
+        User userExists = userService.findUserByEmail(user.getEmail());
+        if (userExists != null) {
+            bindingResult
+                    .rejectValue("email", "error.user",
+                            "There is already a user registered with the username provided");
+        }
+        if (bindingResult.hasErrors()) {
+            modelAndView.setViewName("signup");
+        } else {
+            userService.saveUser(user);
+            modelAndView.addObject("successMessage", "User has been registered successfully");
+            modelAndView.addObject("user", new User());
+            modelAndView.setViewName("login");
+
+        }
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/dashboard", method = RequestMethod.GET)
+    @ResponseBody
+    public ModelAndView dashboard(HttpServletRequest request, Model model, @RequestParam(required = false, name = "email") String emailTo) {
+        ModelAndView modelAndView = new ModelAndView();
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = userService.findUserByEmail(auth.getName());
+        User userTo = userService.findUserByEmail(emailTo);
+
+        if (!request.getSession().isNew()) {
+            //String username = (String) request.getSession().getAttribute("username");
+            model.addAttribute("username", user.getFullname());
+        }
+
+        List<User> allUsersList = userService.getAllUsers();
+
+        modelAndView.addObject("currentUser", user);
+        modelAndView.addObject("fullName", user.getFullname());
+        modelAndView.addObject("adminMessage", "Content Available Only for Users with Admin Role");
+        //modelAndView.addObject("allUsers", user.getAllUsers());
+        modelAndView.addObject("listOfUsers", allUsersList);
+        modelAndView.addObject("emailTo", emailTo);
+        modelAndView.addObject("usernameTo", userTo.getFullname());
+
+        /*for (User s : allUsersList) {
+            System.out.println(s.getFullname().toString());
+        }*/
+
+        //System.out.println (emailTo);
+        modelAndView.setViewName("dashboard");
+        return modelAndView;
+    }
+    
+    @RequestMapping(value = {"/","/home"}, method = RequestMethod.GET)
+    public ModelAndView home(HttpServletRequest request) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("home");
+
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/logout", method = RequestMethod.POST)
+    public ModelAndView logout(HttpServletRequest request) {
+
+        //close WebSocket onLogout and return home page
+        request.getSession(true).invalidate();
+        return home(request);
+    }
+
+}
