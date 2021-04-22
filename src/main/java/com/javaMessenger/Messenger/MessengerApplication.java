2d28c8fe2674ff97e44a99b6d54717346dca4843
dmitry <dmitry.bushmanov@gmail.com>
2021-04-22 23:10:18 +0300

    add Role repo

--- src/main/java/com/javaMessenger/Messenger/MessengerApplication.java
+++ src/main/java/com/javaMessenger/Messenger/MessengerApplication.java
@@ -1,13 +1,38 @@
 package com.javaMessenger.Messenger;
 
+import com.javaMessenger.Messenger.domain.Role;
+import com.javaMessenger.Messenger.repository.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MessengerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MessengerApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(RoleRepository roleRepository) {
+
+		return args -> {
+
+			Role adminRole = roleRepository.findByRole("ADMIN");
+			if (adminRole == null) {
+				Role newAdminRole = new Role();
+				newAdminRole.setRole("ADMIN");
+				roleRepository.save(newAdminRole);
+			}
+
+			Role userRole = roleRepository.findByRole("USER");
+			if (userRole == null) {
+				Role newUserRole = new Role();
+				newUserRole.setRole("USER");
+				roleRepository.save(newUserRole);
+			}
+		};
 	}
 
 
