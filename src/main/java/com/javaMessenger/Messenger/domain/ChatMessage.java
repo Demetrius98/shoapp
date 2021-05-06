53752cea90e9f1c92d312f51008bbfcd666fdda3
dmitry <dmitry.bushmanov@gmail.com>
2021-05-06 19:49:04 +0300

    add ChatMessage domain

---
+++ src/main/java/com/javaMessenger/Messenger/domain/ChatMessage.java
@@ -0,0 +1,73 @@
+package com.javaMessenger.Messenger.domain;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.IndexDirection;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+@Document(collection = "ChatMessage")
+public class ChatMessage {
+    @Id
+    private String id;
+    private MessageType type;
+    private String content;
+    private String sender;
+    private String recipient;
+    @Indexed(unique = true, direction = IndexDirection.DESCENDING, dropDups = true)
+    private Date time;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+    public String getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(String recipient) {
+        this.recipient = recipient;
+    }
+
+    public enum MessageType {
+        CHAT, JOIN, LEAVE
+    }
+
+    public MessageType getType() {
+        return type;
+    }
+
+    public void setType(MessageType type) {
+        this.type = type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+}
