ada80b1e2e2f6bda998884478813f1f3479c8523
dmitry <dmitry.bushmanov@gmail.com>
2021-05-07 23:56:21 +0300

    add webSocketController

---
+++ src/main/java/com/javaMessenger/Messenger/controller/WebSocketController.java
@@ -0,0 +1,48 @@
+package com.javaMessenger.Messenger.controller;
+
+import com.javaMessenger.Messenger.domain.ChatMessage;
+import com.javaMessenger.Messenger.service.*;
+import com.javaMessenger.Messenger.service.ChatMessageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+public class WebSocketController {
+    @Autowired
+    private ChatMessageService messageService;
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/publicChatRoom")
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        //Adding to mongodb
+        messageService.saveMessage(chatMessage);
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/publicChatRoom")
+    public ChatMessage addUser(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+
+        // Add username in web socket session
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat.getMessages")
+    @SendTo("/topic/publicChatRoom")
+    public List<ChatMessage> getMessages(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+
+
+        List<ChatMessage> listOfMessages = messageService.getListOfMessages(chatMessage.getSender(), chatMessage.getRecipient());
+
+        return listOfMessages;
+
+    }
+
+}
